96ea7fdfa6a72ba2782c7f36c5246cb0de19219d
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-03-21 20:54:44 -0700

    add gameboard module

--- scripts.js
+++ scripts.js
@@ -1,4 +1,20 @@
 //Object for gameboard array - use module
+const gameBoard = (() => {
+    //create an array for the gameboard that has cells attached to the last digit of the DOM id
+    const boardCells = [];
+    for (let i = 0; i < 9; i++) {
+        const cell = document.getElementById(`cell${i}`);
+        boardCells.push({
+            id: `cell${i}`,
+            status: cell.dataset.status,
+        });
+        if (cell.dataset.status === 'blank') {
+            cell.addEventListener('click', handleCellClick);
+        } else {
+            cell.removeEventListener('click', handleCellClick);
+        };
+    };
+})();
 
 //Object for players - use factory
 
