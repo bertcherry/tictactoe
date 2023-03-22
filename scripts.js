7c57ff65d275b0ae523fe6f245e8f4bd0767f207
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-03-22 13:40:22 -0700

    add gameplay alternate players

--- scripts.js
+++ scripts.js
@@ -28,26 +28,32 @@ const gameBoard = (() => {
     return {setup, render, boardCells}
 })();
 
-//Object for players - use factory
-const player = (name, marker) => {
-    const getName = () => name;
-    const getMarker = () => marker;
-    return {getName, getMarker};
-};
-
 //Object to control the flow of the game - use module
 const gameController = (() => {
     //pull these out to make game controller standard module language?
     let activePlayer;
+    let playerX;
+    let playerO;
     const startButton = document.querySelector('.submit-btn');
     startButton.addEventListener('click', startGame);
 
     function startGame(e) {
         e.preventDefault();
         document.getElementById('form-container').style.display = 'none';
+        playerSet();
+    };
+
+    //Object for players - use factory
+    const player = (name, marker) => {
+        const getName = () => name;
+        const getMarker = () => marker;
+        return {getName, getMarker};
+    };
+
+    const playerSet = () => {
         //Two inputs for player name, one already assigned to X and one to O
-        const playerX = player(document.getElementById('x-name').value, 'x');
-        const playerO = player(document.getElementById('o-name').value, 'o');
+        playerX = player(document.getElementById('x-name').value, 'x');
+        playerO = player(document.getElementById('o-name').value, 'o');
 
         //Pick a random number to choose which player goes first
         let randomStart = Math.floor(Math.random() * 2);
@@ -55,7 +61,19 @@ const gameController = (() => {
             activePlayer = playerX;
         } else {
             activePlayer = playerO;
-        }
+        };
+        return playerX, playerO;
+    };
+
+    const togglePlayer = activePlayer => {
+        if (activePlayer.getMarker() === 'x') {
+            activePlayer.marker = playerSet(playerO.getMarker());
+            activePlayer.name = playerSet(playerO.getName());
+        } else if (activePlayer.getMarker() === 'o') {
+            activePlayer.marker = playerSet(playerX.getMarker());
+            activePlayer.name = playerSet(playerX.getName());
+        };
+        return activePlayer;
     };
 
     //define handleCellClick function, use exported version in gameboard listeners
@@ -63,8 +81,9 @@ const gameController = (() => {
         //use activePlayer to determine which marker goes on the DOM, and update the cell status to that marker, update gameBoard
         let cellIndex = e.target.getAttribute('id')
         cellIndex = cellIndex.charAt(cellIndex.length - 1);
-        gameBoard.boardCells[cellIndex].status = activePlayer.marker;
+        gameBoard.boardCells[cellIndex].status = activePlayer.getMarker();
         gameBoard.render();
+        togglePlayer(activePlayer);
     };
 
     return {handleCellClick};
